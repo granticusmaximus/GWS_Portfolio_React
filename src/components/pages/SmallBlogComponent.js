0c24209343130d3d01920913630f95835809e1cc
Grant Watson <gwatson117@gmail.com>
2022-06-15 08:32:20 -0600

    Update to css and SmallBlogComponent

--- src/components/pages/SmallBlogComponent.js
+++ src/components/pages/SmallBlogComponent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Card } from 'react-bootstrap'
 import sanityClient from "../../client.js";
 import Pagination from "react-sanity-pagination";
 
@@ -16,6 +17,7 @@ const SmallBlogComponent = () => {
           title,
           slug,
           body,
+          excert,
           author,
           mainImage {
               asset -> {
@@ -35,24 +37,24 @@ const SmallBlogComponent = () => {
     setAllPosts(items);
   }
 
+  const MAX_LENGTH = 150;
+
   return (
     <div className="bg-green-100 min-h p-12">
       <div className="container mx-auto">
         <h2 className="text-5xl flex justify-center cursive">GWS Articles</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {allPostsData && allPostsData.map((post, index) => (
-            <Link to={"/" + post.slug.current} key={post.slug.current}>
-              <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-orange-400" key={index}>
-                <img className="w-full h-full rounded-r object-cover absolute" src={post.mainImage.asset.url} alt="" />
-                <span className="block relative h-full flex justify-end items-end pr -4 pb-4">
-                  <h2 className="text-gray-800 text-lg font-bold px-3 py-4 bg-orange-700 text-orangereact-sanity-pagination-100 bg-opacity-75 rounded">
-                    {post.title}
-                  </h2>
-                </span>
+        {allPostsData && allPostsData.map((post, index) => (
+          <Link to={"/" + post.slug.current} key={post.slug.current}>
+            <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-orange-400" key={index}>
+              <img className="w-full h-full rounded-r object-cover absolute" src={post.mainImage.asset.url} alt="" />
+              <span className="block relative h-full flex justify-end items-end pr -4 pb-4">
+                <h2 className="text-gray-800 text-lg font-bold px-3 py-4 bg-orange-700 text-orangereact-sanity-pagination-100 bg-opacity-75 rounded">
+                  {post.title}
+                </h2>
               </span>
-            </Link>
+            </span>
+          </Link>
           ))}
-        </div>
         <center>
           <Pagination
             nextButton={true}
